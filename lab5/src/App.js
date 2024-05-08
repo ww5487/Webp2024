3375be9bb0febacae04a607636beb0ac8492c281
ww5487 <120004825+ww5487@users.noreply.github.com>
2024-05-08 16:35:21 +0800

    Add files via upload

---
+++ lab5/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import './OX.css';
+import Game from './OX.js';
+
+function App() {
+  return (
+    <div className="root">
+      <Game />
+    </div>
+  );
+}
+
+export default App;
